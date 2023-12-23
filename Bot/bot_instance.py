afba29f324a7a8ff05eba618ea78dcb5a0d02a24
yohans hailu <emailyyohans@gmail.com>
2023-12-23 17:43:44 +0300

    added proxiy for hosting

--- Bot/bot_instance.py
+++ Bot/bot_instance.py
@@ -1,8 +1,14 @@
 from aiogram import Bot
+from aiogram.client.session.aiohttp import AiohttpSession
 from dotenv import load_dotenv
 import os
+
 
 load_dotenv()
 token = os.getenv('API_KEY')
 
-bot = Bot(token=token, parse_mode="HTML")
+bot = Bot(token=token, parse_mode="HTML",
+
+    session=AiohttpSession(proxy='http://proxy.server:3128')
+
+          )
